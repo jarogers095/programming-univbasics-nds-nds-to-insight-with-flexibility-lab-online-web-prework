5e378cde1cde7e249b8ef3c061c4c3f01d60e7fa
James Rogers <jarogers095@gmail.com>
2019-12-02 00:46:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -86,15 +86,15 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   
-  movies_with_directors_aoa = []
+  movies_with_directors_aoh = []
   
   source.each do |director|
     movies_with_director_key(director[:name],director[:movies]).each do |movies_with_name|
-      movies_with_directors_aoa.push(movies_with_name)
+      movies_with_directors_aoh.push(movies_with_name)
     end
   end
   
-  pp movies_with_directors_aoa
+  return movies_with_directors_aoh
 end
 
 # ----------------    End of Your Code Region --------------------
